38e70e9b5738b46fe71788c5d470e87dbca4120e
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-06-24 18:54:14 -0500

    Create a Mock to styles

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = shallow(<Footer />);
+
   test('Render Footer component', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
   });
+  test('Title render', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+
 });
