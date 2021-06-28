a275ba3790d062d4253f6094cec80cfbdc218860
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-06-28 18:35:22 -0500

    Create a Header Test

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('Test <Header />', () => {
+  test('Header component render', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('Title render', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.find('.Header-title').text).toEqual('Platzi Store');
+  });
+});
