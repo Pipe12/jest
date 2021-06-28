e6f52f94ab57369f183f0cffacf910d97a187efe
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-06-28 15:58:53 -0500

    Why I can not use mount here ?

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
