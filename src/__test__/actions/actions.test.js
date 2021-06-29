8d99ea2be6906455cc252922f510ed93444f7322
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-06-29 18:40:24 -0500

    Create tests for actions

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,22 @@
+// import { expect } from '@jest/globals';
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions', () => {
+  test('addToCart Action', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+  test('removeFromCart', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
+});
