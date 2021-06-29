9df606d93ac1f43ccc4966ceddda2247bd2404fb
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-06-28 20:21:32 -0500

    Create a test for product components

---
+++ src/__mocks__/ProductMock.js
@@ -0,0 +1,9 @@
+const ProductMock = {
+  'id': '4',
+  'image': 'https://arepa.s3.amazonaws.com/pin.png',
+  'title': 'Pin',
+  'price': 4,
+  'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+};
+
+export default ProductMock;
